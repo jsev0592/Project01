2eb249c834b8d4d8db7facc034d6f8af21e8d4a9
johndansev <jsev0592@uni.sydney.edu.au>
2017-05-02 11:44:21 +1000

    added power function

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def squared(n):
 	return n * n
 
+def cubed(n):
+	return n * n * n
+
+def power(n, p):
+	return n ** p
+
 def isEven(n):
 	if n % 2 == 0:
 		return "yes"
 	else:
 		return "no"
 
-def cubed(n):
-	return n * n * n
-
 n = 4
 print("squared: " + str(squared(n)))
 print("cubed: " + str(cubed(n)))
 print("isEven: " + isEven(n))
+print("power of 5: " + str(power(n, 5)))
